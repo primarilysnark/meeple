65ebfc848c89c52f2260d9470dbc9a1d09f2e0a8
Primarily Snark <starkey.joshua@gmail.com>
2017-04-23 16:58:57 -0700

    Add wrapper around Roll20 API.
    Added wrapper around Roll20 API to reduce pollution of the global
    namespace with roll20 function.

---
+++ test/lib/roll20.js
@@ -0,0 +1,59 @@
+var test = require('ava');
+var td = require('testdouble');
+
+var api = td.replace('../../lib/helpers/api');
+var roll20 = require('../../lib/helpers/roll20');
+
+test('#getToken returns null for no matching token', t => {
+  td.when(api.findObjs({
+    _type: 'graphic',
+    _subtype: 'token',
+    name: 'bad-token'
+  })).thenReturn([]);
+
+  t.is(roll20.getToken('bad-token'), null);
+});
+
+test('#getToken throws if token is ambiguous', t => {
+  td.when(api.findObjs({
+    _type: 'graphic',
+    _subtype: 'token',
+    name: 'ambiguous-token'
+  })).thenReturn([{}, {}]);
+
+  const error = t.throws(() => {
+    return roll20.getToken('ambiguous-token');
+  }, Error);
+
+  t.is(error.message, 'Too many matching tokens found.');
+});
+
+test('#getToken returns the token if matching token found', t => {
+  td.when(api.findObjs({
+    _type: 'graphic',
+    _subtype: 'token',
+    name: 'good-token'
+  })).thenReturn([{
+    id: 'good-test-id'
+  }]);
+
+  const token = roll20.getToken('good-token');
+  t.is(token.id, 'good-test-id');
+});
+
+test('#getToken returns the token with extra options specified', t => {
+  td.when(api.findObjs({
+    _type: 'graphic',
+    _subtype: 'token',
+    name: 'good-token',
+    testOption: 'test'
+  })).thenReturn([{
+    id: 'good-test-id'
+  }]);
+
+  const token = roll20.getToken('good-token', {
+    testOption: 'test'
+  });
+
+  t.is(token.id, 'good-test-id');
+});
