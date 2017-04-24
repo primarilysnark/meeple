37e72cca2ee5865e3e4be470b7eb452ca2d6d659
Primarily Snark <starkey.joshua@gmail.com>
2017-04-23 17:00:05 -0700

    Add token class for managing tokens.
    Added token class for managing tokens including movement and
    placement.

---
+++ test/lib/token.js
@@ -0,0 +1,66 @@
+var test = require('ava');
+var td = require('testdouble');
+
+var api = td.replace('../../lib/helpers/api');
+var roll20 = td.replace('../../lib/helpers/roll20');
+var Token = require('../../lib/token');
+
+let testToken;
+test.beforeEach(t => {
+  td.when(roll20.getToken('test-token-name', { testOptions: true })).thenReturn({
+    _id: 'test-token-id'
+  });
+
+  testToken = new Token('test-token-name', { testOptions: true });
+});
+
+test('#constructor should create a new Token', t => {
+  td.when(roll20.getToken('good-token-name', { testOptions: true })).thenReturn({
+    _id: 'test-token-id'
+  });
+
+  const token = new Token('good-token-name', { testOptions: true });
+  t.is(token.internalToken._id, 'test-token-id');
+});
+
+test('#constructor should throw if token is missing', t => {
+  td.when(roll20.getToken('missing-token-name', { testOptions: true })).thenReturn(null);
+
+  const error = t.throws(() => {
+    return new Token('missing-token-name', { testOptions: true });
+  }, Error);
+
+  t.is(error.message, 'Token must exist.');
+});
+
+test('#getId should return the token\'s id', t => {
+  t.is(testToken.getId(), 'test-token-id');
+});
+
+test('#move should move the id by the provided coordinates', t => {
+  td.when(api.getTokenProperty(testToken.internalToken, 'left')).thenReturn(0);
+  td.when(api.getTokenProperty(testToken.internalToken, 'top')).thenReturn(2);
+  td.when(api.distanceToPixels(5)).thenReturn(5);
+
+  testToken.move(5, 5);
+
+  td.verify(api.setTokenProperties(testToken.internalToken, {
+    left: 5,
+    top: 7
+  }));
+
+  t.pass();
+});
+
+test('#place should move the id to the provided coordinates', t => {
+  td.when(api.distanceToPixels(5)).thenReturn(5);
+
+  testToken.place(5, 5);
+
+  td.verify(api.setTokenProperties(testToken.internalToken, {
+    left: 5,
+    top: 5
+  }));
+
+  t.pass();
+});
